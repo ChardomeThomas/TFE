da0a2df998825732e01f2d0fb34b23f7bc5d54e2
Thomas Chardome <76436344+ChardomeThomas@users.noreply.github.com>
2024-12-30 12:00:51 +0100

    update jsonservice.ts

--- src/app/core/service/jsonService/json.service.ts
+++ src/app/core/service/jsonService/json.service.ts
@@ -7,21 +7,27 @@ import { Country } from '../../../interface/country.interface';
   providedIn: 'root'
 })
 export class JsonService {
-  // private jsonUrl = 'assets/json/data.json';
   private countries = 'https://thomas-chardome.be/ajout-json/countries.json';
   private cities = 'https://thomas-chardome.be/ajout-json/cities.json';
   private days = 'https://thomas-chardome.be/ajout-json/days.json';
   constructor(private http: HttpClient) { }
-
+  
   getCountries(): Observable<any> {
     return this.http.get<Country[]>(this.countries);
   }
-
+  
   getCities(): Observable<any> {
     return this.http.get<any>(this.cities);
   }
-
+  
   getDays(): Observable<any> {
     return this.http.get<any>(this.days);
   }
+  
+  
+  //pour la carte leaflet
+  // private jsonUrl = 'assets/json/data.json';
+  //    getJsonData(): Observable<any> {
+  //      return this.http.get<Country[]>(this.jsonUrl);
+  //    }
 }
